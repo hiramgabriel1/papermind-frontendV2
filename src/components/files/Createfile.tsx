faef98ca9eeda70bd6bf3e20c106a96de10f0d1b
hiramgabriel-dev <hiram.gabriel@atratopago.com>
2025-03-05 19:38:52 -0700

    a main alaburguer

--- src/components/files/Createfile.tsx
+++ src/components/files/Createfile.tsx
@@ -1,25 +1,36 @@
-import React from 'react'
+import React from "react";
 interface createfileProps {
-    onClose: () => void;
+	onClose: () => void;
 }
 
-
-
-export default function Createfile({onClose}: createfileProps) {
-  return (
-    <div>
-        <div className='flex h-screen w-screen justify-center items-center z-10'>
-            <div className='bg-white p-10 rounded-lg shadow-lg'>
-                <div className='flex justify-around items-center'>
-                    <h3>Crear carpeta</h3>
-                    <button className='text-black p-2 rounded-lg' onClick={onClose}>X</button>
-                </div>
-                <div>
-                    <input type='text' placeholder='Nombre de la carpeta' className='w-full p-2 rounded-lg my-2' />
-                    <button className='bg-blue-500 text-white p-2 rounded-lg w-full'>Crear</button>
-                </div>
-            </div>
-        </div>
-    </div>
-  )
+/**
+ * Componente para crear una carpeta
+ * @param onClose - Función para cerrar el componente
+ * @returns Componente de creación de carpeta
+ */
+export default function Createfile({ onClose }: createfileProps) {
+	return (
+		<div>
+			<div className="flex h-screen w-screen justify-center items-center z-10">
+				<div className="bg-white p-10 rounded-lg shadow-lg">
+					<div className="flex justify-around items-center">
+						<h3>Crear carpeta</h3>
+						<button className="text-black p-2 rounded-lg" onClick={onClose}>
+							X
+						</button>
+					</div>
+					<div>
+						<input
+							type="text"
+							placeholder="Nombre de la carpeta"
+							className="w-full p-2 rounded-lg my-2"
+						/>
+						<button className="bg-black text-white p-2 rounded-lg w-full">
+							Crear
+						</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
--- src/components/files/Document.tsx
+++ src/components/files/Document.tsx
@@ -1,47 +1,57 @@
 "use client";
 
-import React from 'react'
-import { useEffect, useState} from 'react';
-import Createfile from './Createfile';
+import React from "react";
+import { useState } from "react";
+import Createfile from "./Createfile";
 
 export default function Document() {
-    const [isOpen, setIsOpen] = useState(false);
-    const [isClient, setIsClient] = useState(false);
+	const [isOpen, setIsOpen] = useState(false);
 
-  return (
-    <main>
-        <section>
-            <div className='flex justify-between'>
-                <div>
-                    <h3 className='font-semibold text-2xl'>
-                        Documentos
-                    </h3>
-                    <p className='text-gray-500'>
-                        Mira todos tus archivos aqui.
-                    </p>
-                </div>
-                <div className='flex space-x-4 justify-center'>
-                    <div>
-                        <button className='px-3 py-2 border border-gray-500 rounded-lg'>Crear carpeta</button>
-                    </div>
-                    <div>
-                        <button className='px-3 py-2 border border-gray-500 bg-black text-white rounded-lg' onClick={() => setIsOpen(true)}>Subir</button>
-
-                        {isOpen && <Createfile onClose={() => setIsOpen(false)} />}
-                    </div>
-                </div>
-            </div>
-            <div className='flex space-x-3 py-6'>
-                <input type="text" name="search" placeholder='Search' className='border border-gray-500 px-3 rounded-lg text-gray-500'/>
-                <select name="filter" id="filter" defaultValue={1} className='border border-gray-500 px-3 text-gray-500 rounded-lg'>
-                    <option value="1">Todos</option>
-                    <option value="2">Recientes</option>
-                    <option value="3">3 meses antes</option>
-                    <option value="4">6 meses antes</option>
-                </select>
-                
-            </div>
-        </section>
-    </main>
-  )
+	return (
+		<main>
+			<section>
+				<div className="flex justify-between">
+					<div>
+						<h3 className="font-semibold text-2xl">Documentos</h3>
+						<p className="text-gray-500">Mira todos tus archivos aqui.</p>
+					</div>
+					<div className="flex space-x-4 justify-center">
+						<div>
+							<button
+								className="px-3 py-2 border border-gray-500 rounded-lg"
+								onClick={() => setIsOpen(true)}
+							>
+								Crear carpeta
+							</button>
+							{isOpen && <Createfile onClose={() => setIsOpen(false)} />}
+						</div>
+						<div>
+							<button className="px-3 py-2 border border-gray-500 bg-black text-white rounded-lg">
+								Subir
+							</button>
+						</div>
+					</div>
+				</div>
+				<div className="flex space-x-3 py-6">
+					<input
+						type="text"
+						name="search"
+						placeholder="Search"
+						className="border border-gray-500 px-3 rounded-lg text-gray-500"
+					/>
+					<select
+						name="filter"
+						id="filter"
+						defaultValue={1}
+						className="border border-gray-500 px-3 text-gray-500 rounded-lg"
+					>
+						<option value="1">Todos</option>
+						<option value="2">Recientes</option>
+						<option value="3">3 meses antes</option>
+						<option value="4">6 meses antes</option>
+					</select>
+				</div>
+			</section>
+		</main>
+	);
 }
