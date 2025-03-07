87b70b7030cf0f9ae8a6a001820f5cefb1a77340
hiramgabriel-dev <hiram.gabriel@atratopago.com>
2025-03-06 23:09:09 -0700

    removed double table

--- src/app/documents/page.tsx
+++ src/app/documents/page.tsx
@@ -1,30 +1,13 @@
-"use client";
-
-import { useGetDocuments } from "@/hooks/useGetDocuments";
-import { useEffect, useState } from "react";
-import Table from "../../components/files/Table";
 import Document from "../../components/files/Document";
-import { IDocument } from "@/types/Documents.interfaces";
 
 /**
  * This is the main page for the documents
  * @returns
  */
 export default function Documents() {
-	const { getDocuments } = useGetDocuments();
-	const [documents, setDocuments] = useState<IDocument[] | null>(null);
-
-	useEffect(() => {
-		getDocuments()
-			.then((data) => setDocuments(data))
-			.catch();
-	}, []);
-
 	return (
 		<div>
 			<Document />
-			{/* @ts-expect-error ignore type error */}
-			<Table directoryData={documents} />
 		</div>
 	);
 }
